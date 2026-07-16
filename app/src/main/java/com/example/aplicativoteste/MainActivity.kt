dcee55f07a3bb66e9ac7084caa611cae8d474fb1
Leonardo Menezes <leonardomom03@gmail.com>
2026-07-15 21:54:13 -0400

    feat: create initial raffle app

---
+++ app/src/main/java/com/example/aplicativoteste/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.aplicativoteste
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+fun sortear(view : View){
+
+    val textResultado = findViewById<TextView>(R.id.textResultado)
+    val numero =  Random.nextInt(11)
+
+    textResultado.setText("Número sorteado: " + numero)
+}
+
+}
